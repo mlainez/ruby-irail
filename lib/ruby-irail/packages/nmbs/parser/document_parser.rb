874c80c27255902625fe6616c70d245816b26524
Marc Lainez <marc.lainez@gmail.com>
2012-07-17 15:17:30 -0400

    Extract method in nmbs document parser

--- lib/ruby-irail/packages/nmbs/parser/document_parser.rb
+++ lib/ruby-irail/packages/nmbs/parser/document_parser.rb
@@ -3,11 +3,14 @@ module IRail::NMBS
     STATION_XPATH = "//station"
 
     def self.parse_stations(xml_string)
-      xml_payload  = Nokogiri::XML(xml_string)
-      xml_stations = xml_payload.xpath(STATION_XPATH)
-      xml_stations.inject([]) do |stations, xml_station|
+      xml_stations(xml_string).inject([]) do |stations, xml_station|
         stations << IRail::NMBS::Station.from_xml(xml_station.to_s)
       end
+    end
+
+    def self.xml_stations(xml_string)
+      xml_payload = Nokogiri::XML(xml_string)
+      xml_payload.xpath(STATION_XPATH)
     end
   end
 end
