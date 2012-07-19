9431bfa6f47380d507e105f667e59f1701b88d05
Marc Lainez <marc.lainez@gmail.com>
2012-07-19 12:00:40 -0400

    Refactor nmbs document parser

--- lib/ruby-irail/packages/nmbs/parser/document_parser.rb
+++ lib/ruby-irail/packages/nmbs/parser/document_parser.rb
@@ -5,19 +5,19 @@ module IRail::NMBS
     VEHICLE_XPATH    = "//vehicleinformation"
 
     def self.parse_stations(xml_string)
-      xml_stations(xml_string).inject([]) do |stations, xml_station|
+      xml_payload(xml_string, STATION_XPATH).inject([]) do |stations, xml_station|
         stations << IRail::NMBS::Station.from_xml(xml_station.to_s)
       end
     end
 
     def self.parse_connections(xml_string)
-      xml_connections(xml_string).inject([]) do |connections, xml_connection|
+      xml_payload(xml_string, CONNECTION_XPATH).inject([]) do |connections, xml_connection|
         connections << IRail::NMBS::Connection.from_xml(xml_connection.to_s)
       end
     end
 
     def self.parse_vehicle(xml_string)
-      vehicle = xml_vehicle(xml_string)
+      vehicle = xml_payload(xml_string, VEHICLE_XPATH)
       IRail::NMBS::VehicleInformation.from_xml(vehicle.to_s)
     end
 
@@ -25,19 +25,9 @@ module IRail::NMBS
       IRail::NMBS::Liveboard.from_xml(xml_string)
     end
 
-    def self.xml_vehicle(xml_string)
+    def self.xml_payload(xml_string, xpath)
       xml_payload = Nokogiri::XML(xml_string)
-      xml_payload.xpath(VEHICLE_XPATH)
-    end
-
-    def self.xml_stations(xml_string)
-      xml_payload = Nokogiri::XML(xml_string)
-      xml_payload.xpath(STATION_XPATH)
-    end
-
-    def self.xml_connections(xml_string)
-      xml_payload = Nokogiri::XML(xml_string)
-      xml_payload.xpath(CONNECTION_XPATH)
+      xml_payload.xpath(xpath)
     end
   end
 end
